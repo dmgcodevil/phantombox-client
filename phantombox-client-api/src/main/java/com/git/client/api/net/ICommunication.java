1248c232e8497eb20608034df7eaffaa949f1581
dmgcodevil <dmgcodevil@gmail.com>
2012-12-05 00:02:45 +0300

    Added communication interface.

---
+++ phantombox-client-api/src/main/java/com/git/client/api/net/ICommunication.java
@@ -0,0 +1,13 @@
+package com.git.client.api.net;
+
+/**
+ * Communication interface.
+ * <p/>
+ * User: dmgcodevil
+ * Date: 12/1/12
+ * Time: 4:33 AM
+ */
+public interface ICommunication {
+
+// TODO define methods hear
+}
